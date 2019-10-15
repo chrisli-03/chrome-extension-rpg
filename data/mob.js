53ac8a98cf236f6483b8ffc668f8f6823f5cb6fd
Chris <lihecheng93@gmail.com>
2019-10-14 21:10:03 -0400

    changed slime token drop rate

--- data/mob.js
+++ data/mob.js
@@ -4,22 +4,22 @@ const mob = (function() {
     green_slime: {
       name: 'Green Slime',
       hp: 5, atk: 1, def: 0, spd: 0,
-      drop: [{ id: 0, chance: 9500 }, { id: 1000, chance: 1 }]
+      drop: [{ id: 0, chance: 9500 }, { id: 1000, chance: 10 }]
     },
     blue_slime: {
       name: 'Blue Slime',
       hp: 10, atk: 3, def: 1, spd: 2,
-      drop: [{ id: 0, chance: 9500 },  { id: 1000, chance: 2 }]
+      drop: [{ id: 0, chance: 9500 },  { id: 1000, chance: 20 }]
     },
     giant_slime: {
       name: 'Giant Slime',
       hp: 50, atk: 5, def: 15, spd: 3,
-      drop: [{ id: 0, chance: 9500 }, { id: 1, chance: 9500 }, { id: 1000, chance: 5 }]
+      drop: [{ id: 0, chance: 9500 }, { id: 1, chance: 9500 }, { id: 1000, chance: 50 }]
     },
     slime_king: {
       name: 'Slime King',
       hp: 1000, atk: 100, def: 100, spd: 100,
-      drop: [{ id: 0, chance: 9500 }, { id: 0, chance: 9500 },{ id: 0, chance: 9500 }, { id: 1000, chance: 10000 }, { id: 1000, chance: 5000 }]
+      drop: [{ id: 0, chance: 9500 }, { id: 0, chance: 9500 },{ id: 0, chance: 9500 }, { id: 1000, chance: 10000 }, { id: 1000, chance: 6666 }, { id: 1000, chance: 3333 }]
     }
   }
 
